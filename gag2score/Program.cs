faa19a4699dc94d2ef722aa820cba0098b1ae03f
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-10-14 10:04:12 +0900

    [update]gag2score
    csv読み込み追加

--- gag2score/Program.cs
+++ gag2score/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace gag2score
 {
@@ -6,8 +8,26 @@ namespace gag2score
     {
         static void Main(string[] args)
         {
+            string[][] data = loadCSV("../src/dajare_data_10.csv");
+
+            foreach (string[] row in data) {
+                foreach (string elm in row) {
+                    System.Console.Write("{0} ", elm);
+                }
+                    System.Console.Write("\n");
+            }
+
             HumorCalculator hc = new HumorCalculator();
             hc.humorScore("ふとんがふっとんだ");
+        }
+
+        static string[][] loadCSV(string filePath) {
+            var list = new List<string[]>();
+            StreamReader reader = 
+            new StreamReader(filePath, System.Text.Encoding.GetEncoding("UTF-8"));
+            while (reader.Peek() >= 0) list.Add(reader.ReadLine().Split(','));
+            reader.Close();
+            return list.ToArray();
         }
     }
 }
