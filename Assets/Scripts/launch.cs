ea139b4f1439b95beebe433d5ea1275357256b08
Junpei Egusa <al.hsksr@gmail.com>
2020-10-18 19:53:56 +0900

    発射

---
+++ Assets/Scripts/launch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class launch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void pushbutton(){
+        Debug.Log("ボタンを押した");
+        GameObject obj = GameObject.Find("Sphere");  
+        obj.transform.position += Vector3.up;
+    }
+}
