17a669bd9f09cecd14421e431970946c69484be7
Junpei Egusa <al.hsksr@gmail.com>
2020-10-18 21:45:33 +0900

    音声を文字に変換エラーあり

--- Assets/Scripts/launch.cs
+++ Assets/Scripts/launch.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
 
 public class launch : MonoBehaviour
 {
+    [SerializeField]
+    private Text m_Hypotheses;
+
+    [SerializeField]
+    private Text m_Recognitions;
+
+    private DictationRecognizer m_DictationRecognizer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +31,31 @@ public class launch : MonoBehaviour
         Debug.Log("ボタンを押した");
         GameObject obj = GameObject.Find("Sphere");  
         obj.transform.position += Vector3.up;
+         m_DictationRecognizer = new DictationRecognizer();
+
+        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            Debug.LogFormat("Dictation result: {0}", text);
+            m_Recognitions.text += text + "\n";
+        };
+
+        m_DictationRecognizer.DictationHypothesis += (text) =>
+        {
+            Debug.LogFormat("Dictation hypothesis: {0}", text);
+            m_Hypotheses.text += text;
+        };
+
+        m_DictationRecognizer.DictationComplete += (completionCause) =>
+        {
+            if (completionCause != DictationCompletionCause.Complete)
+                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+        };
+
+        m_DictationRecognizer.DictationError += (error, hresult) =>
+        {
+            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+        };
+
+        m_DictationRecognizer.Start();
     }
 }
