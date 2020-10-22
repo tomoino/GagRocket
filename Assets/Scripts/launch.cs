23938956d3a753bfd707d5ca321a2f9fadfbc754
Junpei Egusa <al.hsksr@gmail.com>
2020-10-22 19:47:31 +0900

    音声認識ほぼ完成

--- Assets/Scripts/launch.cs
+++ Assets/Scripts/launch.cs
@@ -8,20 +8,32 @@ using UnityEngine.Windows.Speech;
 
 public class launch : MonoBehaviour
 {
-    [SerializeField]
-    private Text m_Hypotheses;
-
-    [SerializeField]
-    private Text m_Recognitions;
-
     private DictationRecognizer m_DictationRecognizer;
 
     private int flag = 0;
+    private string resultText;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_DictationRecognizer = new DictationRecognizer();
+
+        m_DictationRecognizer.DictationResult += (text, confidence) =>
+        {
+            Debug.LogFormat("Dictation result: {0}", text);
+            resultText = text;
+        };
+
+        m_DictationRecognizer.DictationComplete += (completionCause) =>
+        {
+            if (completionCause != DictationCompletionCause.Complete)
+                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+        };
+
+        m_DictationRecognizer.DictationError += (error, hresult) =>
+        {
+            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+        };
     }
 
     // Update is called once per frame
@@ -36,36 +48,11 @@ public class launch : MonoBehaviour
 
         if(flag == 0){
             flag = 1;
-
-         m_DictationRecognizer = new DictationRecognizer();
-
-        m_DictationRecognizer.DictationResult += (text, confidence) =>
-        {
-            Debug.LogFormat("Dictation result: {0}", text);
-            m_Recognitions.text += text + "\n";
-        };
-
-        m_DictationRecognizer.DictationHypothesis += (text) =>
-        {
-            Debug.LogFormat("Dictation hypothesis: {0}", text);
-            m_Hypotheses.text += text;
-        };
-
-        m_DictationRecognizer.DictationComplete += (completionCause) =>
-        {
-            if (completionCause != DictationCompletionCause.Complete)
-                Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
-        };
-
-        m_DictationRecognizer.DictationError += (error, hresult) =>
-        {
-            Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
-        };
-
-        m_DictationRecognizer.Start();
+            m_DictationRecognizer.Start();
         }
         else{
             m_DictationRecognizer.Stop();
+            Debug.LogFormat("result: {0}",resultText);
         }
     }
 }
