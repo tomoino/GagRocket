a0bc1018da0edc3d623dd3c03203861c0e532cab
Junpei Egusa <al.hsksr@gmail.com>
2020-10-25 16:57:44 +0900

    スコアに応じてロケット加速

--- Assets/Scripts/launch.cs
+++ Assets/Scripts/launch.cs
@@ -15,6 +15,10 @@ public class launch : MonoBehaviour
 
     public GameObject dajare_object = null;
 
+    public GameObject rocket_object = null;
+
+    private float speed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -41,13 +45,19 @@ public class launch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        // GameObject obj = GameObject.Find("AtomRocket");
+        // if(flag == 2 && obj.transform.position.y<score){
+        //     obj.transform.position += Vector3.up;
+        // }
+
+        if(flag == 2){
+            Rigidbody rb = rocket_object.GetComponent<Rigidbody>();  
+            Vector3 force = new Vector3 (0.0f,speed,0.0f);
+            rb.AddForce (force);
+        } 
+    }   
     public void pushbutton(){
         Debug.Log("ボタンを押した");
-        GameObject obj = GameObject.Find("AtomRocket");  
-        obj.transform.position += Vector3.up;
-
         if(flag == 0){
             flag = 1;
             m_DictationRecognizer.Start();
@@ -56,6 +66,10 @@ public class launch : MonoBehaviour
             m_DictationRecognizer.Stop();
             Debug.LogFormat("result: {0}",resultText);
             dajare();
+            //ダジャレをスコア化（floatで0~1が返ってくる)
+            float score = 0.3f;
+            speed *= score;
+            flag = 2;
         }
     }
     public void dajare(){
@@ -63,4 +77,19 @@ public class launch : MonoBehaviour
         Text dajare_text = dajare_object.GetComponent<Text>(); 
         dajare_text.text = resultText;
     }
+
+    // public void speed(){
+    //     Rigidbody rb = rocket_object.GetComponent<Rigidbody>();  
+    //     Vector3 force = new Vector3 (0.0f,30.0f,0.0f);
+    //     rb.AddForce (force);  
+    // }
+
+    //  for文だと処理スピードが速くて加速にならない。
+    // public void speed(float score){
+    //     Rigidbody rb = rocket_object.GetComponent<Rigidbody>();  
+    //     Vector3 force = new Vector3 (0.0f,score*200,0.0f);
+    //     for(int i=0;i<score*100;i++){
+    //         rb.AddForce (force);
+    //     }
+    // }
 }
