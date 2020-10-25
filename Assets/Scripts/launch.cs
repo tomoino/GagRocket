e17984312142654fadb67f076e551cd5ad872ed3
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-10-25 22:14:28 +0900

    [add]api
    flaskでスコア計算のAPIを作成

--- Assets/Scripts/launch.cs
+++ Assets/Scripts/launch.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 using UnityEngine.Windows.Speech;
 using Gag2Score;
 
+using UnityEngine.Networking;
 
 public class launch : MonoBehaviour
 {
@@ -15,6 +16,9 @@ public class launch : MonoBehaviour
     private string resultText;
 
     public GameObject dajare_object = null;
+    
+    private const string URL = "http://localhost:5000/";
+    public string text = "犬 が 居 ぬ";
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +41,8 @@ public class launch : MonoBehaviour
         {
             Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
         };
+
+        StartCoroutine (Connect ());
     }
 
     // Update is called once per frame
@@ -44,8 +50,49 @@ public class launch : MonoBehaviour
     {
         
     }
+
+    
+    private IEnumerator Connect(){
+        // WWWForm form = new WWWForm();
+        // form.AddField("text", "犬 が 居 ぬ");
+        string myjson = JsonUtility.ToJson(this);
+        byte[] postData = System.Text.Encoding.UTF8.GetBytes (myjson);
+        var request = new UnityWebRequest(URL, "POST");
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(postData);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.Send();
+
+        // UnityWebRequest request = UnityWebRequest.Get(URL);
+        // yield return request.SendWebRequest();
+
+        // //エラー処理
+        // if(request.isNetworkError){
+        //     Debug.Log(request.error);
+        // }else{
+        //     //リクエストが成功した時
+        //     if(request.responseCode == 200){
+        //         Debug.LogFormat("result: {0}", request.downloadHandler.text);
+        //     }
+        // }
+
+            // yield return www.SendWebRequest();
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+                Debug.LogFormat("result: {0}", request.downloadHandler.text);
+            }
+    }
+
     public void pushbutton(){
         Debug.Log("ボタンを押した");
+        
+
         HumorCalculator hc = new HumorCalculator();
         double result = hc.humorScore("布団が吹っ飛んだ");
         Debug.LogFormat("HC: {0}",result);
